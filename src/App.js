47f185d5b26fd86902c33ca0c042c98e97351540
Gafar <104564468+iGafar@users.noreply.github.com>
2023-10-23 12:51:29 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./components/Main";
+import Routes from "./routes";
+import Main from "./components/Main"
 
 function App() {
   const [activeTasks, setActiveTasks] = React.useState(0);
---
+++ src/routes.jsx
@@ -0,0 +1,10 @@
+// import { Routes, Route } from "react-router-dom"
+// import Main from "./components/Main"
+// import TaskDescription from "./components/TaskDescription"
+
+// // export default () => (
+// //   <Routes>
+// //     <Route path="/" element={<Main/>}/>
+// //     <Route path="edit" element={<TaskDescription/>}/>
+// //   </Routes>
+// // )
